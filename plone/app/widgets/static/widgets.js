0ee32cd8323b4a5f203aefcc17453ca4ca1eb1ee
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-09-07 12:33:19 +0300

    Sync fixes from mockup branch

--- plone/app/widgets/static/widgets.js
+++ plone/app/widgets/static/widgets.js
@@ -32312,7 +32312,6 @@ define('mockup-patterns-relateditems',[
 
       self.$el.parents('.pattern-relateditems-container').parents().on('click', function(){
           self.$el.select2('close');
-          return false;
       });
 
       $('button.mode.search', self.$toolbar).on('click', function(e) {
